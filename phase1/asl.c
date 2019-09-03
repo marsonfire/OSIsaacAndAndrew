dd57f5edf740dae8da93c09466fb4af9cf0e167a
Issac Nasser <nasseri@cerebro.cs.xu.edu>
2019-09-03 16:43:02 -0400

    initASL structure

--- phase1/asl.c
+++ phase1/asl.c
@@ -3,7 +3,8 @@
 #include "../h/types.h"
 #include "../e/pcb.e"
 
-HIDDEN semd_t *semdFree_h;
+HIDDEN semd_t *semdFree_h;    /*Free semaphore list*/
+HIDDEN semd_t *semdActive_h;  /*Active semaphore list*/
 
 int insertBlocked (int *semAdd, pcb_PTR p){
 
@@ -22,6 +23,17 @@ pcb_PTR headBlocked (int *semAdd){
 }
 
 void initASL (){
-  static semd_t semdTable[MAXPROC];
+  static semd_t semdTable[MAXPROC];/*unsure if needed*/
+  /*I believe we need to fill this +1/2 due to dummy nodes*/
+
+  /*Initialize both lists as NULL*/
+  semdFree_h = NULL;
+  semdActive_h = NULL;
+
+  /*fill free list*/
+  int i;
+  for(i=0;i<MAXPROC;i++){
+    /*semd add and link function to be written*/
+  }
   
 }
